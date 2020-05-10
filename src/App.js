5970c4b7881087f0d70f4deb71281bd01ff1b5cd
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-05-10 15:30:55 -0400

    change the app functional component to an app class component. added a state for robots and the search field. changed the cardlist prop to render the robots from the robots state, not the robots object

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-	return(
-		<div className='tc'>
-			<h1>RoboFriends</h1>
-			<SearchBox />
-			<CardList robots={robots} />
-		</div>
-	)
-}
+export default class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			robots: robots,
+			searchField: ''
+		}
+	}
 
-export default App;
+	render() {
+		return(
+			<div className='tc'>
+				<h1>RoboFriends</h1>
+				<SearchBox />
+				<CardList robots={this.state.robots} />
+			</div>
+		)
+	}
+}
