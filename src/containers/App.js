be6522f8b1bed08e6ba337dfd59552b5fea300d1
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-08-15 13:47:40 -0400

    removed the react state from app. updated componentDidMount to render the robots passed down from props. updated the return conditional

--- src/containers/App.js
+++ src/containers/App.js
@@ -25,27 +25,18 @@ const mapDispatchToProps = dispatch => {
 }
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-		}
-	}
-
 	componentDidMount() {
-		
-			// .then(users => this.setState({ robots: users }));
+		this.props.onRequestRobots();
 	}
 
 	render() {
-		const { robots } = this.state;
-		const { searchField, onSearchChange } = this.props;
-
+		const { searchField, onSearchChange, robots, isPending } = this.props;
+		
 		const filteredRobots = robots.filter(robot => {
 			return robot.name.toLocaleLowerCase().includes(searchField.toLocaleLowerCase());
 		})
 
-		return !robots.length ?
+		return isPending ?
 			<h1 className='tc f1'>Loading...</h1> :
 			(
 				<div className='tc'>
